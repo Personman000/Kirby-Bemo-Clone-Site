a9bf836aa646206a48cdb93637488e8d780360d8
Saad Ali <saad_syed_ali@hotmail.com>
2020-04-30 23:05:36 -0400

    Added everything for first page

---
+++ site/snippets/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <?= css(['assets/css/index.css', '@auto']) ?>
+
+    <title></title>
+</head>
+
+
+<body>
+	<header id="navbar">
+		<img id="logo" src="<?= url('assets/images/bemo-logo.png') ?>">
+		<nav id="menu">
+			<?php foreach ($pages->listed() as $pg): ?>
+	        	<?= $pg->title()->link() ?>
+	        <?php endforeach ?>
+	    </nav>
+	</header>
+	<div id="coverwrapper">
+		<img id="cover" src="<?= $page->image()->url() ?>"></img>
+		<div id="heading"><h1><?= $page->heading() ?></h1></div>
+	</div>
---
+++ site/templates/home.php
@@ -0,0 +1,9 @@
+<?php snippet('header') ?>
+
+<div id="maintext">
+	<p>
+		<?= $page->text()->kirbytext() ?>
+	</p>
+</div>
+
+<?php snippet('footer') ?>
